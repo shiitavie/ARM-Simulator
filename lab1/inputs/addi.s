afa9ce900fdb7f310c1a31c908eeedfcb8dd1e77
Stevie Xie <66903483+shiitavie@users.noreply.github.com>
2023-03-26 15:12:29 -0500

    removed old directories

--- lab1/inputs/addi.s
+++
@@ -1,7 +0,0 @@
-.text
-add X10, X0, 0xfff
-add X0, X10, 0xfff
-add X1, X11, 11
-add X2, X12, 12
-
-HLT 0
--- lab4/cleaning.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm diff_*.txt reference*.txt lab4-test.txt src/actual*.txt curr_diff_*.txt dumpsim src/dumpsim actual*.txt
